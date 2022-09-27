915d98d788a78a0143aa51bc1fa6d68f853b32bd
KIMINJOONG <ijboym216@gmail.com>
2022-09-28 08:43:15 +0900

    1. 스프링 데이터 JPA와 DB설정, 동작확인

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,37 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        // jpa 특성상 같은 transaction안에서는 영속성 컨텍스트의 동일성을 보장함 즉 같은 인스턴스(1차 캐시)
+        Assertions.assertThat(findMember).isEqualTo(member); // true
+
+
+
+
+
+
+    }
+
+}
